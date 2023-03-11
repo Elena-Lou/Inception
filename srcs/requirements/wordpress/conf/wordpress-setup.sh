b2dbd6867811329f6626dba9821687fa1f123ee4
Elena-Lou <elouisiade@live.fr>
2023-03-11 19:04:34 +0100

    WIP WORDPRESS SETUP :
    wp-config file is moved into wordpress folder and the wordpress
    compressed folder is removed

--- srcs/requirements/wordpress/conf/wordpress-setup.sh
+++ srcs/requirements/wordpress/conf/wordpress-setup.sh
@@ -2,11 +2,16 @@
 
 curl -LO https://wordpress.org/latest.tar.gz
 tar xzvf latest.tar.gz
+rm -rf latest.tar.gz
 chown -R www-data:www-data /var/www/wordpress
+
 i=0
 while [ $i -le 7 ];
 do
 	sed -i "0,/put your unique phrase here/s//`pwgen -s 64`/" wp-config.php
 	((i++))
 done
+
+mv wp-config.php wordpress/
+
 exec "$@"
