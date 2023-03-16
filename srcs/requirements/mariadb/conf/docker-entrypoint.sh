83f7acd673d5d49487ffeb1855abce1cc7be03b3
Elena-Lou <elouisiade@live.fr>
2023-03-16 17:05:13 +0100

    WIP DATABASE SCRIPT:
    The script prevents everything from working... damn you script...

--- srcs/requirements/mariadb/conf/docker-entrypoint.sh
+++ srcs/requirements/mariadb/conf/docker-entrypoint.sh
@@ -4,19 +4,15 @@ mysql_install_db
 
 service mysql start
 
-mysql -e "UPDATE mysql.user SET password = PASSWORD('IMANEWPWD') WHERE user = 'root';"
+#mysql -e "SELECT user,authentication_string,plugin,host FROM mysql.user;"
 
-mysql -e "DROP USER ''@'localhost';"
+mysql -e "DELETE FROM mysql.user WHERE user='' ;"
 
-mysql -e "DROP USER ''@'$(hostname)';"
+#mysql -e "DROP USER ''@'$(hostname)';"
 
 mysql -e "DROP DATABASE test;"
 
-mysql -e "FLUSH PRIVILEGES;"
-
-#mysql -e "SELECT user,authentication_string,plugin,host FROM mysql.user;"
-
-#mysql -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root';"
+echo "\n --- DataBase dropped --- \n"
 
 mysql -e "SELECT user,authentication_string,plugin,host FROM mysql.user;"
 
@@ -24,12 +20,26 @@ mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 mysql -e "SHOW DATABASES;"
 
+echo "\n --- creation user Bouh --- \n"
+
 mysql -e "CREATE USER IF NOT EXISTS 'bouh'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 mysql -e "SELECT user FROM mysql.user;"
 
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'bouh'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
+echo "\n --- user bouh receives all privileges on wordpress --- \n"
+
+mysql -e "SELECT user,authentication_string,plugin,host FROM mysql.user;"
+
+#mysql -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root';"
+
+#mysql -e "UPDATE mysql.user SET password = PASSWORD('IMANEWPWD') WHERE user = 'root';"
+
+echo "\n --- password set for root user --- \n"
+
 mysql -e "FLUSH PRIVILEGES;"
+
+service mysql stop
 
 exec "$@"
