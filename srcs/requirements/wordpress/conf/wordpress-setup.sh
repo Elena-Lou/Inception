27099ccf35511416b561c1931bcbb728878a4f71
Elena-Lou <elouisiade@live.fr>
2023-03-24 16:59:47 +0100

    WIP WORDPRESS DOCKERFILE AND SCRIPT:
    Wordpress site is up and running with two different users
    Files need to be cleaned up

--- srcs/requirements/wordpress/conf/wordpress-setup.sh
+++ srcs/requirements/wordpress/conf/wordpress-setup.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
-#curl -LO https://wordpress.org/latest.tar.gz
-#tar xzvf latest.tar.gz
-#rm -rf latest.tar.gz
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
 
-wp core download
-ls 
-#chown -R root:root /var/www/wordpress
+	echo BEFORE DOWNLOAD 
 
-#i=0
-#while [ $i -le 7 ];
-#do
-#	sed -i "0,/put your unique phrase here/s//`pwgen -s 64`/" wp-config.php
-#	((i++))
-#done
+	wp core download --allow-root \
+		--path='/var/www/wordpress'
+	
+	echo CORE DOWNLOAD COMPLETE
+	
+	echo BEFORE CONFIG CREATE
+	
+	wp config create --allow-root \
+		--dbname=$MYSQL_DATABASE \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/wordpress/'
 
-#mv wp-config.php wordpress/
-#rm wordpress/wp-config-sample.php
+	echo AFTER CONFIG CREATE
+
+	echo BEFORE CORE INSTALL
+
+	wp core install --allow-root \
+		--url=$WP_URL \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMIN_PWD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--skip-email \
+		--path='/var/www/wordpress/'
+
+	echo AFTER CORE INSTALL
+
+	echo BERFORE USER CREATE
+
+	wp user create --allow-root \
+		$WP_USER \
+		$WP_USER_EMAIL \
+		--user_pass=$WP_USER_PWD \
+		--role=author \
+		--path='/var/www/wordpress/'
+	
+	echo AFTER USER CREATE 
+else 
+	echo /var/www/wordpress/wp-config.php already created 
+
+fi
 
 exec "$@"
