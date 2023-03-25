effea198f12681937e2ad6bb9fc32c272680c44c
Elena-Lou <elouisiade@live.fr>
2023-03-25 16:13:00 +0100

    ADD CLEANED UP MARIADB SCRIPT:
    Script is clearer with the use of a heredoc instead of a lists
    of commands

--- srcs/requirements/mariadb/conf/docker-entrypoint.sh
+++ srcs/requirements/mariadb/conf/docker-entrypoint.sh
@@ -10,28 +10,24 @@ while [ ! -e /var/run/mysqld/mysqld.sock ]; do
 	sleep 2
 done
 
-set -x 
-
 echo "service is ready"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql << _EOF_
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 
+ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;
 
-mysql -u -root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;
 
-set +x
+_EOF_
 
-sleep 3
-
-#service mysql stop
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
 exec $@
